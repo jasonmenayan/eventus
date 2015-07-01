758e369b0814e1a13b55b24124b406f23ad5865d
Jason Menayan <jasonmenayan@gmail.com>
2015-07-01 15:43:36 -0700

    continued work on eventbriteapi.js - committing to rebase

--- app/www/js/Services/eventbriteapi.js
+++ app/www/js/Services/eventbriteapi.js
@@ -5,19 +5,10 @@ angular.module('starter')
     // post potential user to server which will determine if legit
 
     var llurlbase = 'http://maps.googleapis.com/maps/api/geocode/json?address=';
-    var tzurlbase = 'https://maps.googleapis.com/maps/api/timezone/json?location=';
+    // var tzurlbase = 'https://maps.googleapis.com/maps/api/timezone/json?location=';
     var eburlbase = 'https://www.eventbriteapi.com/v3/events/search/?';
-    var ebtoken = '';
+    var ebtken = 'MKFL4P7OGOVRM3P7IV6L';
     var fetchedEvents = {};
-
-    // should accept an object with following format:
-    // parameters = {
-    //   zip: 55555,
-    //   miwithin: 5, // # of miles within ZIP code
-    //   categories: '101,103,105', // comma-delimited IDs
-    //   start_begin: 'yyyy-mm-ddThh:mm:00Z', // start_date.range_start; convert to UTC
-    //   start_end: 'yyyy-mm-ddThh:mm:00Z' // start_date.range_end; convert to UTC
-    // }
 
     fetchEvents.getLatLongFromZip = function(zip) {
       var url = gmurlbase + zip;
@@ -29,39 +20,62 @@ angular.module('starter')
           lat = results[0].geometry.location.lat;
           lng = results[0].geometry.location.lng;
         }
+        return {lat: lat, lng: lng};
       }).
-      .error(function(results, status) {
+      .error(function (results, status) {
         console.log('Lat/lng not fetched. Error: ', status);
       });
-      return {lat: lat, lng: lng};
     };
 
-    fetchEvents.getTimeZoneFromLatLng = function(latlng, unixtime) {
-      var lat = latlng.lat;
-      var lng = latlng.lng;
-      var url = tzurlbase + lat + ',' + lng + '&timestamp=' + unixtime;
-      var offsetHrs = 0;
-      return $http.get(url)
-      .success(function (results, status) {
-        if (status === 'OK') {
-          offsetHrs = (results.dstOffset + results.rawOffset) / 3600;
-        }
-      }).
-      .error(function(results, status) {
-        console.log('Time zone offset not fetched. Error: ', status);
-      });
-      return offsetHrs;
-    };
+    // fetchEvents.getTimeZoneFromLatLng = function(latlng) {
+    //   var lat = latlng.lat;
+    //   var lng = latlng.lng;
+    //   var unixts = (new Date).getTime();
+    //   var url = tzurlbase + lat + ',' + lng + '&timestamp=' + unixts;
+    //   var offsetHrs = 0;
+    //   return $http.get(url)
+    //   .success(function (results, status) {
+    //     if (status === 'OK') {
+    //       offsetHrs = (results.dstOffset + results.rawOffset) / 3600;
+    //     }
+    //     return offsetHrs;
+    //   }).
+    //   .error(function(results, status) {
+    //     console.log('Time zone offset not fetched. Error: ', status);
+    //   });
+    // };
+
+  // should accept an object with following format:
+    // parameters = {
+    //   zip: 55555,
+    //   miwithin: 5, // # of miles within ZIP code
+    //   categories: '101,103,105', // comma-delimited IDs
+    // }
 
     fetchEvents.getEvents = function (parameters) {
-      var lat = getLatLongTimeZoneFromZip(parameters.zip).lat;
-      var lng = getLatLongTimeZoneFromZip(parameters.zip).lng;
       var mi = parameters.miwithin;
       var cats = parameters.categories;
-      var begin = parameters.start_begin;
-      var end = parameters.start_end;
-      var url = llurlbase + params;
-      return $http.get(url)
+      var lat = '';
+      var lng = '';
+      var eventArr = [];
+
+      getLatLongFromZip(parameters.zip)
+        .then(function (latlong) {
+          lat = latlong.lat;
+          lng = latlong.lng;
+          var url = llurlbase + 'location.within=' + mi + 'mi&location.latitude=' + lat + '&location.longitude=' + lng + '&start_date.keyword="this_month"&categories=' + cats + '&token=' + ebtken;
+          $http.get(url)
+          .then(function (results) {
+            eventArr.push(results.events);
+            if (results.pagination.page_number !== results.pagination.page_count) {
+              // recursively call: append '&page=' + results.pagination.page_number+1
+            }
+          })
+        })
+      
+
+
+
       .success(function (results, status) {
         fetchedEvents = results;
       });
