959926a4032e6eae45515e4e01a3a51aec9fe747
Jason Menayan <jasonmenayan@gmail.com>
2015-07-01 18:53:03 -0700

    eventbriteapi - ready to test

--- app/www/js/Services/eventbriteapi.js
+++ app/www/js/Services/eventbriteapi.js
@@ -5,13 +5,11 @@ angular.module('starter')
     // post potential user to server which will determine if legit
 
     var llurlbase = 'http://maps.googleapis.com/maps/api/geocode/json?address=';
-    // var tzurlbase = 'https://maps.googleapis.com/maps/api/timezone/json?location=';
     var eburlbase = 'https://www.eventbriteapi.com/v3/events/search/?';
     var ebtken = 'MKFL4P7OGOVRM3P7IV6L';
-    var fetchedEvents = {};
 
     fetchEvents.getLatLongFromZip = function(zip) {
-      var url = gmurlbase + zip;
+      var url = llurlbase + zip;
       var lat = '';
       var lng = '';
       return $http.get(url)
@@ -26,24 +24,6 @@ angular.module('starter')
         console.log('Lat/lng not fetched. Error: ', status);
       });
     };
-
-    // fetchEvents.getTimeZoneFromLatLng = function(latlng) {
-    //   var lat = latlng.lat;
-    //   var lng = latlng.lng;
-    //   var unixts = (new Date).getTime();
-    //   var url = tzurlbase + lat + ',' + lng + '&timestamp=' + unixts;
-    //   var offsetHrs = 0;
-    //   return $http.get(url)
-    //   .success(function (results, status) {
-    //     if (status === 'OK') {
-    //       offsetHrs = (results.dstOffset + results.rawOffset) / 3600;
-    //     }
-    //     return offsetHrs;
-    //   }).
-    //   .error(function(results, status) {
-    //     console.log('Time zone offset not fetched. Error: ', status);
-    //   });
-    // };
 
   // should accept an object with following format:
     // parameters = {
@@ -63,28 +43,24 @@ angular.module('starter')
         .then(function (latlong) {
           lat = latlong.lat;
           lng = latlong.lng;
-          var url = llurlbase + 'location.within=' + mi + 'mi&location.latitude=' + lat + '&location.longitude=' + lng + '&start_date.keyword="this_month"&categories=' + cats + '&token=' + ebtken;
-          $http.get(url)
-          .then(function (results) {
-            eventArr.push(results.events);
-            if (results.pagination.page_number !== results.pagination.page_count) {
-              // recursively call: append '&page=' + results.pagination.page_number+1
-            }
-          })
-        })
-      
+          var url = eburlbase + 'location.within=' + mi + 'mi&location.latitude=' + lat + '&location.longitude=' + lng + '&start_date.keyword=this_month&popular=on&categories=' + cats + '&token=' + ebtken + '&expand=venue';
 
-
-
-      .success(function (results, status) {
-        fetchedEvents = results;
-      });
+          var retrieveEvents = function(url) {
+            $http.get(url)
+            .then(function (results) {
+              eventArr.concat(results.events);
+              if (results.pagination.page_number !== results.pagination.page_count) {
+                var newUrl = url + '&page=' + (+results.pagination.page_number + 1);
+                retrieveEvents(newUrl);
+              } else {
+                return eventArr;
+              }
+            })
+          };
+      });  
+      retrieveEvents(url);
     };
-
-
     
-
-
     return fetchEvents;
 
   }]);
