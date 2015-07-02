5b2b4d3807b45ae0fa6cfd41f877097c4a9f27f4
Sean Connor <seantconnor@gmail.com>
2015-07-02 16:05:07 -0700

    refactored get events.  git should work now

--- app/www/js/Services/eventbriteapi.js
+++ app/www/js/Services/eventbriteapi.js
@@ -1,15 +1,15 @@
 "use strict";
 angular.module('starter')
 
-  .factory("fetchEvents", ['$http', function($http){
+  .factory("FetchEvents", ['$http', function($http){
     // post potential user to server which will determine if legit
 
     var llurlbase = 'http://maps.googleapis.com/maps/api/geocode/json?address=';
     var eburlbase = 'https://www.eventbriteapi.com/v3/events/search/?';
     var ebtken = 'MKFL4P7OGOVRM3P7IV6L';
-    var fetchEvents = {};
+    var FetchEvents = {};
 
-    fetchEvents.getLatLongFromZip = function(zip) {
+    FetchEvents.getLatLongFromZip = function(zip) {
       var url = llurlbase + zip;
       return $http.get(url)
       .success(function (results, status) {
@@ -27,34 +27,26 @@ angular.module('starter')
     //   categories: '101,103,105', // comma-delimited IDs
     // }
 
-    fetchEvents.getEvents = function (parameters) {
-      var mi = parameters.miwithin;
-      var cats = parameters.categories;
-      var eventArr = [];
+    FetchEvents.getEvents = function(parameters, page) {
+      return FetchEvents.getLatLongFromZip(parameters.zip)
+      .then(function(results) {
+        var mi = parameters.miwithin;
+        var cats = parameters.categories;
+        var eventArr = [];
 
-      fetchEvents.getLatLongFromZip(parameters.zip)
-        .then(function (results) {
         var lat = results.data.results[0].geometry.location.lat;
         var lng = results.data.results[0].geometry.location.lng;
-        var urlstem = eburlbase + 'location.within=' + mi + 'mi&location.latitude=' + lat + '&location.longitude=' + lng + '&start_date.keyword=this_month&popular=on&categories=' + cats + '&token=' + ebtken + '&expand=venue';
-
-        var retrieveEvents = function(url) {
-          $http.get(url)
-          .then(function (results) {
-            eventArr = eventArr.concat(results.data.events);
-            if (results.data.pagination.page_number <= results.data.pagination.page_count) {
-              var newUrl = urlstem + '&page=' + (+results.data.pagination.page_number + 1);
-              retrieveEvents(newUrl);
-            } else {
-              return eventArr;
-            }
+        var url = eburlbase + 'location.within=' + mi + 'mi&location.latitude=' + lat + 
+        '&location.longitude=' + lng + '&start_date.keyword=this_month&popular=on&categories=' + 
+        cats + '&token=' + ebtken + '&expand=venue' + '&page=' + page;
+        return $http.get(url)
+          .then(function(results) {
+            FetchEvents.events = results.data.events;
           })
-        };
-      retrieveEvents(urlstem);
-      });  
-    };
-    
-    return fetchEvents;
+      });
+    };    
+
+    return FetchEvents;
 
   }]);
 
